89e437b0fb6e90c82d34e3d4bddc694ec69c26ce
abir <cleverprogrammer404@gmail.com>
2024-06-28 18:42:29 +0600

    First problem solved

---
+++ index.js
@@ -0,0 +1,9 @@
+// 1) Write a function named ‘calculateSum’ that takes two arguments and returns their sum.
+
+function calculateSum(num1, num2) {
+  let sum = num1 + num2;
+  return sum;
+}
+
+console.log(calculateSum(10, 20));
+console.log(calculateSum(10, 10));
