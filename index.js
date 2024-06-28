7a7ea6ca41b2899dc678d89889e13d974cc20d7b
abir <cleverprogrammer404@gmail.com>
2024-06-28 19:25:28 +0600

    Third problem solved

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 1) Write a function named ‘calculateSum’ that takes two arguments and returns their sum.
+/* // 1) Write a function named ‘calculateSum’ that takes two arguments and returns their sum.
 
 function calculateSum(num1, num2) {
   let sum = num1 + num2;
@@ -20,3 +20,21 @@ function isEven(num) {
 
 console.log(isEven(6));
 console.log(isEven(9));
+*/
+
+// 3) Write a function named ‘findMax’ that takes an array of numbers and returns the largest number in the array.
+
+function findMax(numArray) {
+  let max = numArray[0];
+
+  for (let i = 1; i < numArray.length; i++) {
+    if (numArray[i] > max) {
+      max = numArray[i];
+    }
+  }
+
+  return max;
+}
+
+let numbers = [5, 1, 4, 3, 18, 7, 2, 9];
+console.log(findMax(numbers));
